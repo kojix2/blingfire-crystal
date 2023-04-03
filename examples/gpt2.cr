ac15a5d8320ea984127ef2aa91f3e140a934baf3
kojix2 <2xijok@gmail.com>
2023-04-03 14:15:14 +0900

    Fix example/gpt2.cr

--- examples/gpt2.cr
+++ examples/gpt2.cr
@@ -1,16 +1,20 @@
 require "../src/blingfire"
+require "colorize"
+
+Dir.cd("#{__DIR__}/../spec/fixtures")
 
 # Load the model
 model = BlingFire::Model.new("gpt2.bin")
 
 # Get the text
 text = "Intelligence is an accident of evolution, and not necessarily an advantage."
+puts "- text: #{text.colorize(:green)}"
 
 # Tokenize the text
 tokens = model.text_to_ids(text)
 
 # Print the tokens
-puts tokens
+puts "- text2token: #{tokens.colorize(:green)}"
 
 # Token to text
 model = BlingFire::Model.new("gpt2.i2w")
@@ -19,5 +23,4 @@ model = BlingFire::Model.new("gpt2.i2w")
 text = model.ids_to_text(tokens)
 
 # Print the text
-puts text
-
+puts "- token2text: #{text.colorize(:green)}"
